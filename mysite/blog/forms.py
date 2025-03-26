a2603cc09341a684e455727cfe758501de02daac
OIITuMuCT <vataman@live.ru>
2025-03-26 10:03:12 +0300

    Creating forms form models CommtentForm

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 
+from .models import Comment
+
 
 class EmailPostForm(forms.Form):
+    """ Форма для отправки поста по e-mail """
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+class CommentForm(forms.ModelForm):
+    """ Форма для комментария """
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
