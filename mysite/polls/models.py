7d72cfd8357e47607fbf97205fb6f50b8f6df98d
OIITuMuCT <vataman@live.ru>
2025-03-26 18:40:43 +0300

    initial commit

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=250)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question_text
+
+class Answer(models.Model):
+    title = models.CharField(max_length=250)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=250)
+    votes = models.IntegerField(default=0)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Question
 # Create your views here.
+def home(request):
+    questions = Question.objects.all()
+    return render(request, 'polls/home.html', {'questions': questions})
