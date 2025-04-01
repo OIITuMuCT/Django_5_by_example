aacfcbf02f31d78e0648f39b19d7fdf064f909f4
OIITuMuCT <vataman@live.ru>
2025-04-01 18:39:17 +0300

    Displaying the activity stream

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 
+from actions.utils import create_action
+from actions.models import Action
 from .models import Contact, Profile
 from .forms import (
     LoginForm,
@@ -17,10 +19,18 @@ from .forms import (
 @login_required
 def dashboard(request):
     """Dashboard view"""
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list(
+        'id', flat=True
+    )
+    if following_ids:
+        # if user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions[:10]
     return render(
         request,
         'account/dashboard.html',
-        {'section': 'dashboard'}
+        {'section': 'dashboard', 'actions': actions}
     )
 
 def user_login(request):
@@ -61,6 +71,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(
                 request,
                 'account/register_done.html',
@@ -143,6 +154,7 @@ def user_follow(request):
                     user_form=request.user,
                     user_to=user
                 )
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(
                     user_form=request.user,
