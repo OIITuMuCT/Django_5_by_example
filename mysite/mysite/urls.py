8c156ed1b6d353aea45d5a2555b948f70a110c23
OIITuMuCT <vataman@live.ru>
2025-04-03 12:59:44 +0300

    Adding router and register URLs

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,8 +18,17 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 from blog.sitemaps import PostSitemap
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'posts', views.PostViewSet)
+router.register(r'comments', views.CommentViewSet)
 
 sitemaps = {
     'posts': PostSitemap,
@@ -35,4 +44,6 @@ urlpatterns = [
         {'sitemaps':sitemaps},
         name='django.contrib.sitemaps.views.sitemap'
     ),
-]
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+] + debug_toolbar_urls()
