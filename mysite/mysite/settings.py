afb75aaeb26e80707bd9f55867c002f6af5fe896
OIITuMuCT <vataman@live.ru>
2025-04-03 13:00:12 +0300

    Adding settings for rest_framework

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,12 +40,17 @@ INSTALLED_APPS = [
     "django.contrib.sites",
     "django.contrib.sitemaps",
     "django.contrib.staticfiles",  # фреймворк управления статическими файлами# фреймворк сеансов
+    "debug_toolbar",
     "taggit",
     "blog.apps.BlogConfig",
     "django.contrib.postgres",
+    "rest_framework",
+    "api.apps.ApiConfig",
+    
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -142,3 +147,17 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+# rest_framework config
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
