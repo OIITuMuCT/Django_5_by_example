30c08b72d98e8cd2facfae98ad6a93f28dc54099
OIITuMuCT <vataman@live.ru>
2025-04-03 19:43:59 +0300

    add snippets app

---
+++ tutorial/snippets/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+# Create your views here.
