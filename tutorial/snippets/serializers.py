ebda008fe8a71abe9ce338c853d024069fd4e813
OIITuMuCT <vataman@live.ru>
2025-04-04 15:41:04 +0300

    Tuturial 5: Relationships & Hyperlinked APIs

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -3,29 +3,25 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all()
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name="snippet-highlight", format="html"
     )
 
     class Meta:
-        model = User
-        fields = ["id", "username", "snippets"]
-
-
-class SnippetSerializer(serializers.ModelSerializer):
-
-    class Meta:
         model = Snippet
-        fields = ["id", "title", "code", "linenos", "language", "style"]
-        owner = serializers.ReadOnlyField(source="owner.username")
-
-    # id = serializers.IntegerField(read_only=True)
-    # title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    # code = serializers.CharField(style={'base_template': 'textarea.html'})
-    # linenos = serializers.BooleanField(required=False)
-    # language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    # style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+        fields = [
+            "url",
+            "id",
+            "highlight",
+            "owner",
+            "title",
+            "code",
+            "linenos",
+            "language",
+            "style",
+        ]
 
     def create(self, validated_data):
         """
@@ -44,3 +40,13 @@ class SnippetSerializer(serializers.ModelSerializer):
         instance.style = validated_data.get("stile", instance.style)
         instance.save()
         return instance
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name="snippet-detail", read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = ["url", "id", "username", "snippets"]
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,8 +1,12 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework import generics, permissions
 from django.contrib.auth.models import User
+from rest_framework import renderers
+from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
 class UserList(generics.ListAPIView):
@@ -21,8 +25,26 @@ class SnippetList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {
+            "users": reverse("user-list", request=request, format=format),
+            "snippets": reverse("snippet-list", request=request, format=format),
+        }
+    )
