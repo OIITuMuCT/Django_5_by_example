35744d1e64154c1ef1ca42edf13e45feff1d625a
OIITuMuCT <vataman@live.ru>
2025-04-08 12:26:27 +0300

    Creating models Subject, Course, Module

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Subject(models.Model):
+    """ Предмет, к которому относится курс"""
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        """ Возвращает название предмета """
+        return self.title
+
+class Course(models.Model):
+    """ Модель курса, которая содержит модули """
+    owner = models.ForeignKey(
+        User,
+        related_name='course_created',
+        on_delete=models.CASCADE
+    )
+    subject = models.ForeignKey(
+        Subject,
+        related_name='courses',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        """ Возвращает название курса """
+        return self.title
+
+class Module(models.Model):
+    """ Модель модуля для курса """
+    course = models.ForeignKey(
+        Course,
+        related_name='modules',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        """ Возвращает название модуля """
+        return self.title
