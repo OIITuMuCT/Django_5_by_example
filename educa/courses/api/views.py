66ecbad4c0d9d177c692c04affb15e80917f21da
OIITuMuCT <vataman@live.ru>
2025-04-11 22:52:32 +0300

    Building list and detail views

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,11 +1,15 @@
 from rest_framework import generics
+from django.db.models import Count
+
+from courses.api.pagination import StandardPagination
 from courses.api.serializers import SubjectSerializer
 from courses.models import Subject
 
 class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.all()
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
 
 class SubjectDetailView(generics.RetrieveAPIView):
-    queryset = Subject.objects.all()
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
