57cd4bb8b3a081ff9a26509521482184f9c36064
OIITuMuCT <vataman@live.ru>
2025-05-07 14:33:06 +0300

    Rendering PDF files

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -7,6 +7,13 @@ from django.urls import reverse
 
 from .models import Order, OrderItem
 
+
+def order_pdf(obj):
+    url = reverse("orders:admin_order_pdf", args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+
+order_pdf.short_description = "Invoice"
+
 def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
@@ -52,6 +59,7 @@ def order_payment(obj):
 
 order_payment.short_description = 'Stripe payment'
 
+
 # Register your models here.
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -68,6 +76,7 @@ class OrderAdmin(admin.ModelAdmin):
         "created",
         "updated",
         order_detail,
+        order_pdf,
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
